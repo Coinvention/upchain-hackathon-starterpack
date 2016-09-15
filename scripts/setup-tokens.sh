388d712c452fd5b25832407682b5d30d3d089aa6
Sebastian Schürmann <sebs@2xs.org>
2016-09-15 12:09:17 +0200

    Omit build, rm folders and copy data

--- scripts/setup-tokens.sh
+++ scripts/setup-tokens.sh
@@ -2,4 +2,10 @@ git clone git@github.com:Upchain/Tokens.git  .starterpack/Tokens
 rm -rf app/*
 rm -rf build/*
 rm -rf contracts/*
+rm -rf migrations/*
 rm -rf test/*
+cp -rf .starterpack/Tokens/contracts/* contracts/.
+cp -rf .starterpack/Tokens/app/* app/.
+cp -rf .starterpack/Tokens/contracts/* contracts/.
+cp -rf .starterpack/Tokens/migrations/* migrations/.
+cp -rf .starterpack/Tokens/test/* test/.
